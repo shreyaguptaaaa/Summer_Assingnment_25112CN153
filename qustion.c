aa82726cce4fb27b744037ff86d5a592eeb0fe4b
Surabhi <surabhig.506@gmail.com>
2026-06-03 21:20:15 +0530

    Day 3 assigment

---
+++ qustion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if(count == 2)
+        printf("Prime Number");
+    else
+        printf("Not a Prime Number");
+
+    return 0;
+}
---
+++ qustion2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int start, end, i, j, count;
+
+    printf("Enter starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter ending number: ");
+    scanf("%d", &end);
+
+    printf("Prime numbers are:\n");
+
+    for(i = start; i <= end; i++)
+    {
+        count = 0;
+
+        for(j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+            {
+                count++;
+            }
+        }
+
+        if(count == 2)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
