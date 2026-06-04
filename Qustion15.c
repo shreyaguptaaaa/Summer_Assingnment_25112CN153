c0a01c5257c31320b0b91a4347edbdf9ee9539ce
Surabhi <surabhig.506@gmail.com>
2026-06-04 16:58:24 +0530

    Day 4 upload

---
+++ Qustion15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num, originalNum, remainder;
+    int result = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        result += remainder * remainder * remainder;
+        num /= 10;
+    }
+
+    if (result == originalNum)
+        printf("%d is an Armstrong number.", originalNum);
+    else
+        printf("%d is not an Armstrong number.", originalNum);
+
+    return 0;
+}
